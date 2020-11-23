efe9e59a8e17ce16a7629315959259e5e6e3d53e
Kleimak <mehdi1.mammad@epitech.eu>
2020-11-23 21:38:04 +0100

    add : choseDogoCommand && nodemon && gitignore && dependencies in package.json // fix : package.json

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const client = new Discord.Client;
 
 const prefix = "!";
 
+var giphy = require('giphy-api')('SSj5vSIjFIClh0zj9DayvGvnIkZjs2CW');
+
 client.on("ready", () => {
     console.log("Bot ON");
 });
@@ -13,44 +15,61 @@ client.on("guildMemberAdd", member => {
     member.guild.channels.cache.find(channel => channel.id === "435549024687357962").send("Unv ouveau Singe entre dans le Zoo !! !\nNous sommes désormais **" + member.guild.memberCount + "** dans le Zoo!!");
     member.roles.add("435552439597400065");
 });
-    
-client.on("message", message =>{
-    if(message.author.bot) return;
-//=============================Reponse au Rôle Dog======================>
-    if(message.member.roles.cache.find(r => r.name === "Dog")){
-        message.reply({embed: 
-            {color: 3447003,
-            description: "SALE CHIEN",
-            "image": {url:"https://media.giphy.com/media/1d7F9xyq6j7C1ojbC5/giphy.gif"}
-        }});  
-    }
-//===============================ADMIN PERM=================================>
-    if(message.member.hasPermission("ADMINISTRATOR")){
 
-        if(message.content.startsWith(prefix + "dog")){
+client.on("message", message => {
+    if (message.author.bot) return;
+    //=============================Reponse au Rôle Dog======================>
+    if (message.member.roles.cache.find(r => r.name === "Dog")) {
+        message.reply({
+            embed: {
+                color: 3447003,
+                description: "SALE CHIEN",
+                "image": {
+                    url: "https://media.giphy.com/media/1d7F9xyq6j7C1ojbC5/giphy.gif"
+                }
+            }
+        });
+    }
+    //===============================ADMIN PERM=================================>
+    if (message.member.hasPermission("ADMINISTRATOR")) {
+
+        if (message.content.startsWith(prefix + "dog")) {
             let mention = message.mentions.members.first();
             mention.roles.add("780142415914467329");
             mention.voice.setChannel("780142696429518898")
         }
-//================================MOVE TO AUTHOR=================================>
-        if(message.content.startsWith(prefix + "aupied")){
+        //================================MOVE TO AUTHOR=================================>
+        if (message.content.startsWith(prefix + "aupied")) {
             let author = message.member;
             let mention = message.mentions.members.first();
-            mention.voice.setChannel(author.voice.channelID);     
+            mention.voice.setChannel(author.voice.channelID);
         }
-//==============================GIF DE DOG=================================>
-        if(message.content.startsWith(prefix + "kawai")){
-            message.reply({embed: 
-                {color: 3447003,
-                description: "BARK BARK",
-                "image": {url:"https://media.giphy.com/media/aFTt8wvDtqKCQ/giphy.gif"}
-            }});     
-        }
+
     }
-//==========================NON ADMIN=======================================>    
-    if(message.content.startsWith(prefix + "meute")){
-            message.channel.send('<@310088962595225610> <@434488582422528010> <@131413017827999744> <@239855037902225409> <@310086739060326401> BARK??');     
-        }
+    //==========================NON ADMIN=======================================>    
+    if (message.content.startsWith(prefix + "meute")) {
+        message.channel.send('<@310088962595225610> <@434488582422528010> <@131413017827999744> <@239855037902225409> <@310086739060326401> BARK??');
+    }
+
+    //==============================GIF DE DOG=================================>
+    if (message.content.startsWith(prefix + "chosedogo")) {
+        let dogoTeam = ['<@310088962595225610>', '<@434488582422528010>', '<@131413017827999744>', '<@239855037902225409>', '<@310086739060326401>']
+
+        giphy.random('funny dogs', function (err, res) {
+
+            let dog = dogoTeam[Math.floor(Math.random() * dogoTeam.length)];;
+            console.log(res.data);
+            message.channel.send({
+                embed: {
+                    color: 3447003,
+                    description: dog,
+                    "image": {
+                        url: res.data.image_original_url
+                    }
+                }
+            });
+        });
+    }
 });
 
 client.login("NzgwMTI0OTQ0Nzk3MjcwMDM2.X7qh-A.h-PvTCskdzWnZPdKkbHQHK18A24");
